32c784bbbbb103b04dea46d5a4a8f48b78fa0211
Misha Brukman <mbrukman@google.com>
2017-07-26 10:16:40 -0400

    Convert top-of-file comments to doc strings.

--- stackoverflow/24023916/images.py
+++ stackoverflow/24023916/images.py
@@ -13,10 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-################################################################################
-#
-# Update VM OS images and short names.
+
+"""Update VM OS images and short names."""
 
 import argparse
 import glob
--- stackoverflow/24023916/yaml2json.py
+++ stackoverflow/24023916/yaml2json.py
@@ -13,12 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-################################################################################
-#
-# Converts a YAML file to a JSON format, to be consumed by Packer.
-#
-################################################################################
+
+"""Converts a YAML file to a JSON format to be consumed by Packer."""
+
 
 import json
 import yaml
